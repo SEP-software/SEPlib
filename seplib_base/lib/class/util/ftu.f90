b84287673dd2c1e5d8a755e0387cf68a4abfb904
stew <stew@sep.stanford.edu>
2017-02-08 15:03:27 -0800

    Deploy FFTW rather than original power-of-two FFT.

--- seplib_base/lib/class/util/ftu.f90
+++ seplib_base/lib/class/util/ftu.f90
@@ -1,5 +1,8 @@
 subroutine ftu( signi, nx, cx )
-use sep_func_mod
+!
+ use, intrinsic :: iso_c_binding
+!
+!use sep_func_mod
 !   complex fourier transform with unitary scaling
 !
 !               1         nx          signi*2*pi*i*(j-1)*(k-1)/nx
@@ -9,46 +12,25 @@ use sep_func_mod
 integer nx, i, j, k, m, istep, pad2
 real    signi, scale, arg
 complex cx(nx), cmplx, cw, cdel, ct
-if ( nx .ne. pad2(nx) ) then
-  call erexit('ftu: nx not a power of 2')
-end if
+!
+ include 'fftw3.f03'
+!
+type(C_PTR) :: plan
+!
+if (signi > 0.0) then
+  j=1
+else
+  j=-1
+end if 
+plan=fftwf_plan_dft_1d(nx,cx,cx,j, FFTW_ESTIMATE)
+!
+call fftwf_execute_dft(plan,cx,cx)
 scale = 1. / sqrt( 1.*nx)
 do i= 1, nx
   cx(i) = cx(i) * scale
 end do 
-j = 1
-k = 1
-do i= 1, nx  
-  if (i<=j) then
-    ct = cx(j)
-    cx(j) = cx(i)
-    cx(i) = ct
-  end if
-  m = nx/2
-  do while  (j>m .and. m>1)
-    j = j-m
-    m = m/2
-  end do 
-! "&&" means .AND.
-  j = j+m
-end do 
-do   
-  istep = 2*k
-  cw = 1.
-  arg = signi*3.14159265/k
-  cdel = cmplx( cos(arg), sin(arg))
-  do m= 1, k  
-    do i= m, nx, istep
-      ct=cw*cx(i+k)
-      cx(i+k)=cx(i)-ct
-      cx(i)=cx(i)+ct
-    end do 
-    cw = cw * cdel
-  end do 
-  k = istep
-  if (k>=nx) then
-    exit
-  end if
-end do 
+!
+call fftwf_destroy_plan(plan)
+!
 return
 end  
