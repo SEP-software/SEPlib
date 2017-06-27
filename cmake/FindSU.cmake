e44247e9e8ce2c12d2b92009eaa1471c09478dfd
stew <stew@sep.stanford.edu>
2017-06-27 11:26:02 -0700

    Use standard CWPROOT environment variable for detection.

--- cmake/FindSU.cmake
+++ cmake/FindSU.cmake
@@ -12,9 +12,9 @@ endif (SU_INCLUDES)
 
 find_path (SU_INCLUDES su.h)
 
-find_library (SU_LIB HINTS ${CWP_ROOT}  NAMES su)
-find_library (SUCWP_LIB HINTS ${CWP_ROOT}  NAMES cwp)
-find_library (SUPAR_LIB HINTS ${CWP_ROOT}  NAMES par)
+find_library (SU_LIB HINTS ${CWPROOT}  NAMES su)
+find_library (SUCWP_LIB HINTS ${CWPROOT}  NAMES cwp)
+find_library (SUPAR_LIB HINTS ${CWPROOT}  NAMES par)
 if(SU_LIB)
 if(SUCWP_LIB)
 if(SUPAR_LIB)
