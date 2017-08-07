f3e976af6d53388703421052804f1c2e72935442
stew <stew@sep.stanford.edu>
2017-08-07 08:48:53 -0700

    Include sep3d.h rather than jsut seplib.h to pull in missing prototype.

--- seplib_java/lib/getnumberaxes_jni.c
+++ seplib_java/lib/getnumberaxes_jni.c
@@ -1,7 +1,7 @@
 /* native method for GetNumberAxes class static methods sep_get_number_data_axes() */
 #include "sepConfig.h"
 #include "edu_stanford_sep_seplib_corelibs_sep_GetNumberAxes.h"
-#include <seplib.h>
+#include <sep3d.h>
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
