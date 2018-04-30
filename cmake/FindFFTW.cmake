a78aa06cfbaab5ff8f7c29af1d3c6491917af6c7
stew <stew@sep.stanford.edu>
2018-04-30 09:17:39 -0700

    tweaks to handle FFTW linkage.

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -15,18 +15,13 @@ find_path (FFTWF_INCLUDES fftw3.f03)
 
 find_library (FFTW_LIB NAMES fftw3)
 find_library (FFTWF_LIB NAMES fftw3f)
-if(FFTWF_INCLUDES)
-if(FFTW_LIB)
-set(FFTW_LIBRARIES ${FFTW_LIB})
-endif(FFTW_LIB)
-if(FFTWF_LIB)
-set(FFTWF_LIBRARIES   ${FFTWF_LIB} )
-endif(FFTWF_LIB)
-endif(FFTWF_INCLUDES)
+#
+set(FFTW_LIBRARIES ${FFTWF_LIB} ${FFTW_LIB})
+set(FFTWF_LIBRARIES ${FFTWF_LIB} ${FFTW_LIB})
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (FFTW DEFAULT_MSG FFTWF_LIBRARIES FFTW_LIBRARIES FFTW_INCLUDES)
+find_package_handle_standard_args (FFTW DEFAULT_MSG FFTWF_LIBRARIES FFTW_LIBRARIES FFTWF_INCLUDES FFTW_INCLUDES)
 
-mark_as_advanced (FFTWF_LIBRARIES FFTW_LIBRARIES FFTW_INCLUDES )
+mark_as_advanced (FFTWF_LIBRARIES FFTW_LIBRARIES FFTWF_INCLUDES FFTW_INCLUDES )
