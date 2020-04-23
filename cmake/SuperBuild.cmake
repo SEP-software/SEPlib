9383fe0480728fefbfe91162c2b0b72e2c67c8cc
Robert Clapp <bob@roberts-mbp-3.lan>
2020-04-22 20:58:01 -0700

    add superbuild

--- cmake/SuperBuild.cmake
+++ cmake/SuperBuild.cmake
@@ -7,17 +7,17 @@ set (DEPENDENCIES)
 set (EXTRA_CMAKE_ARGS)
 
 # Use static linking to avoid issues with system-wide installations of Boost.
-list (APPEND DEPENDENCIES flex_build)
-ExternalProject_Add (flex_build
-  URL https://github.com/westes/flex/files/981163/flex-2.6.4.tar.gz
-  CONFIGURE_COMMAND ./configure  --prefix=${CMAKE_INSTALL_PREFIX}
-  BUILD_COMMAND make 
+ExternalProject_Add (xorg_macros
+  URL  https://github.com/freedesktop/xorg-macros/archive/util-macros-1.19.1.tar.gz
+  CONFIGURE_COMMAND autoreconf -vif && ./configure  --prefix=${CMAKE_INSTALL_PREFIX}
+  BUILD_COMMAND make
   BUILD_IN_SOURCE 1
   INSTALL_COMMAND make install
   )
 ExternalProject_Add (xaw_build
   URL https://github.com/freedesktop/xorg-libXaw/archive/libXaw-1.0.13.tar.gz
-  CONFIGURE_COMMAND autoreconf -vif && ./configure  --prefix=${CMAKE_INSTALL_PREFIX}
+  DEPENDS xorg_macros
+  CONFIGURE_COMMAND autoreconf -vif -I ${CMAKE_INSTALL_PREFIX}/share/aclocal  && ./configure  --prefix=${CMAKE_INSTALL_PREFIX}
   BUILD_COMMAND make
   BUILD_IN_SOURCE 1
   INSTALL_COMMAND make install
