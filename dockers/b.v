4ff6345faee5851ad186ac8de62bc18226d48ce5
Robert Clapp <bob@roberts-mbp-3.lan>
2020-08-14 15:07:15 -0700

    fix dockers

--- dockers/b.v
+++
@@ -1,6 +0,0 @@
-Fake header for special device-dependent data only.
-To get input history passed along, over-ride default with head = stdout
-	in=/dev/null
-Sxtpen: (warning) cannot read input pipe
-Sxtpen:  No input?
-	n3=0
